664364359de9ce69b09de60d724dca0ac072b946
hooman898589 <hooman8985@gmail.com>
2026-01-03 14:41:55 +0330

    add model to structure

--- model/customer.php
+++ model/customer.php
@@ -5,20 +5,20 @@ use Model\crud;
 class customer{
     use crud;
     
-    protected $filabel=[
-        'firstname',
-        'lastname',
-        'phone',
-    ];
 
     
 public function __construct(){
     
-    $this->set_setting('customers',[
+    $this->set_setting(
+        'customers',
+    
+        [
         'firstname',
         'lastname',
         'phone',
-    ]);
+        ]
+
+);
 }
    
 
